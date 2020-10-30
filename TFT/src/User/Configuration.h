deab7c3b2097800be94823c558abf51827333d80
Manan <mananchavda.9@gmail.com>
2020-10-30 15:46:38 +0530

    colour change

--- TFT/src/User/Configuration.h
+++ TFT/src/User/Configuration.h
@@ -62,7 +62,7 @@
  *          6: MAGENTA,    7: YELLOW,      8: ORANGE,  9: PURPLE,   10: LIME,  11: BROWN,
  *         12: DARKBLUE,  13: DARKGREEN,  14: GRAY,   15: DARKGRAY
  */
-#define MARLIN_BKCOLOR 1
+#define MARLIN_BKCOLOR 4
 #define MARLIN_FNCOLOR 0
 
 /**
